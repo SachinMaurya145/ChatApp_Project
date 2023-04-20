e103d34db0207f885a390ad424b5b3a20e5cb8b9
Sachin Maurya <MauryaSachin145@gmail.com>
2023-04-20 12:54:18 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, { useState } from 'react';
+import ReactECharts from 'echarts-for-react';
+import Data from './JsonData'
 
 function App() {
+
+  const flavanoidsData = Data.map((val) => {
+    return val.Flavanoids;
+  })
+
+  const ashData = Data.map((val) => {
+    return val.Ash;
+  })
+
+  const alcoholData = Data.map((val) => {
+    return val.Alcohol;
+  })
+
+  const magnesiumData = Data.map((val) => {
+    return val.Magnesium;
+  })
+
+
+  const firstChart = {
+    grid: { top: 8, right: 8, bottom: 24, left: 36 },
+    xAxis: {
+      type: 'category',
+      data: flavanoidsData,
+    },
+    yAxis: {
+      type: 'value',
+    },
+    series: [
+      {
+        data: ashData,
+        type: 'line',
+        smooth: true,
+      },
+    ],
+    tooltip: {
+      trigger: 'axis',
+    },
+  };
+
+  const secondChart = {
+    grid: { top: 8, right: 8, bottom: 24, left: 36 },
+    xAxis: {
+      type: 'category',
+      data: alcoholData,
+    },
+    yAxis: {
+      type: 'value',
+    },
+    series: [
+      {
+        data: magnesiumData,
+        type: 'line',
+        smooth: true,
+      },
+    ],
+    tooltip: {
+      trigger: 'axis',
+    },
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2 >Chart to be drawn between “Flavanoids” on the horizontal axis and “Ash” on the
+        vertical axis </h2>
+      <ReactECharts option={firstChart} />
+      <h2 >“Alcohol” categories on the horizontal axis and the
+        minimum “Magnesium” value for each alcohol category on the vertical axis </h2>
+      <ReactECharts option={secondChart} />
     </div>
   );
-}
+};
 
 export default App;
